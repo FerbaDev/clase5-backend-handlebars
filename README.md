749e422aec9851c21d077c1ded02052ee6580dea
FerbaDev <elferbarron1@gmail.com>
2024-02-25 17:52:38 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Clase 5 backend - parte 2
+
+#### () => {}
---
+++ src/app.js
@@ -0,0 +1,28 @@
+import express from "express"
+const app = express();
+const PUERTO = 8080;
+import exphbs from "express-handlebars"
+
+//configuramos handlebars en 3 lineas
+app.engine("handlebars", exphbs.engine()); //le digo a express que cuando encuentre un archivo con la ext handlebars lo tiene que renderizar con el motor de plantillas
+app.set("view engine", "handlebars"); //aca le decimos que el motor es handlebars
+app.set("views", "./src/views"); //aca le pasamos la ruta para que los encuentre
+
+//rutas
+app.get("/", (req, res) => {
+    const usuario = {
+        nombre: "Fer",
+        apellido: "Barron",
+        mayorEdad: true
+
+    }
+    res.render("home", {titulo: "Trabajando handlebars", usuario});
+})
+
+app.get("/contacto", (req, res) => {
+    res.render("contacto", {titulo: "Contacto"} )
+})
+
+app.listen(PUERTO, () => {
+    console.log("escuchando");
+})
